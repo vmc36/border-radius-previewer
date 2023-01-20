2092dae05f754eb15bbc0c2c55de08fa6edae905
Vinicius <vmcmodesto@gmail.com>
2023-01-19 22:44:16 -0300

    feature:fix string px in valueChart

--- src/components/ValueChart/index.jsx
+++ src/components/ValueChart/index.jsx
@@ -1,9 +1,26 @@
 export const ValueChart = ({ borderRadius }) => {
-  const valueBorderRadius = `border-radius: ${borderRadius.topLeft}px ${borderRadius.topRight}px ${borderRadius.bottomRight}px ${borderRadius.bottomLeft}px;`;
+  const valueBorderRadius = `border-radius: ${
+    borderRadius.topLeft != 0
+      ? borderRadius.topLeft + "px"
+      : borderRadius.topLeft
+  } ${
+    borderRadius.topRight != 0
+      ? borderRadius.topRight + "px"
+      : borderRadius.topRight
+  } ${
+    borderRadius.bottomRight != 0
+      ? borderRadius.bottomRight + "px"
+      : borderRadius.bottomRight
+  } ${
+    borderRadius.bottomLeft != 0
+      ? borderRadius.bottomLeft + "px"
+      : borderRadius.bottomLeft
+  };`;
 
   const clipboardValue = () => {
     navigator.clipboard.writeText(valueBorderRadius);
   };
+
   return (
     <>
       <div className="my-5 flex h-[51px] min-w-[286px] justify-between whitespace-nowrap rounded-md bg-chartColor p-3 text-center font-Ubuntu-Mono text-white">
